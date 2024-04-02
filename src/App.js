99b7d09d306c5b1b728d5b47025d3e8a1b395956
Tahir Khan <tahirdecoy786@gmail.com>
2024-04-02 21:50:45 +0530

    add wallet show balance and tailwind css

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import { useWeb3React } from '@web3-react/core';
 import { injected } from './component/connectors';
+import Web3 from 'web3';
+import React, { useState } from "react";
+
+
 
 function App() {
-  const { activate, account, deactivate, active, chainId } = useWeb3React()
-  const connectWallet = async () => {
+  const { activate, account, deactivate, active, chainId} = useWeb3React()
+  const [ethBalance, setEthBalance] = useState([]);
+  
+  const connectWallet = async  () => {
     try {
       await activate(injected)
-    } catch (error) {
+      window.web3 = new Web3(window.ethereum);
+      const accounts =await window.ethereum.request({method: 'eth_requestAccounts'});
+      let weiBalance = await window.web3.eth.getBalance(accounts[0]);
+      let ethBalance = window.web3.utils.fromWei(weiBalance, "ether");
+      setEthBalance(ethBalance)
+    } 
+    catch (e) 
+    {
+     
+      console.log(e)
 
     }
   }
   const disconnectWallet = async () => {
     try {
       await deactivate()
-    } catch (error) {
-
+    } 
+    catch (error) {
     }
   }
   return (
@@ -25,7 +40,7 @@ function App() {
 
       <br />
       {active ?
-        <button className="disconnectwallet" onClick={disconnectWallet}>Disconnect Wallet</button>
+        <button className="disconnectwallet " onClick={disconnectWallet}>Disconnect Wallet</button>
 
         :
         <button className="connectwallet" onClick={connectWallet}>Connect Wallet</button>
@@ -37,16 +52,16 @@ function App() {
       <br />
       {active &&
         <>
-        <p>Wallet Address : <span className='account'>{account}</span></p>
+        <p className='text-[20px] text-red-900'>Wallet Address : <span className='account'>{account}</span></p>
           
           <br />
-          <p>Chain Id : <span className='chainid'>{chainId}</span></p>
+          <p className='text-[20px] text-blue-900'>Chain Id : <span className='chainid'>{chainId}</span></p>
+          <br/>
+         
+          <p className='text-[20px] text-red-900'>Balance : <span className='balance'>{ethBalance}</span></p>
 
         </>
       }
-
-
-
 
     </div>
   );
